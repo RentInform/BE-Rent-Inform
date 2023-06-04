889555bdb3ad4d64dab0dfafa08775f8c232813a
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-04 18:47:16 -0400

    fix: remove commented out lines from serializer

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -5,12 +5,4 @@ class PropertySerializer
   set_id :id
   attributes :street, :city, :state, :walk_score, :bike_score, :transit_score, :safety_score
   attribute :zip, &:zipcode
-
-  # attribute :city do |object|
-  #   object.city = "Philadelphia"
-  # end
-
-  # attribute :state do |object|
-  #   object.state = "PA"
-  # end
 end
