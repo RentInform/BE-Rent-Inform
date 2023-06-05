27c463838bf1c46311142f00c205e9658ac5e238
Logan Cole <emtlogancole@gmail.com>
2023-06-05 17:34:03 -0500

    feat: user_property#show method
    Co-authored-by: Stephen McPhee <stephen.t.mcphee@gmail.com>

--- app/controllers/api/v0/user_properties_controller.rb
+++ app/controllers/api/v0/user_properties_controller.rb
@@ -3,4 +3,9 @@ class Api::V0::UserPropertiesController < ApplicationController
     user_properties = UserPropertyFacade.new.get_properties(params[:user_id])
     render json: UserPropertySerializer.new(user_properties)
   end
+
+  def show
+    user_property = UserPropertyFacade.new.get_property(params[:user_id], params[:property_id])
+    render json: UserPropertySerializer.new(user_property)
+  end
 end
