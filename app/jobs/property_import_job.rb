fbac3f4e3035d779caa0b9b37516185d82840515
Grace Joh <graceehjoh@gmail.com>
2023-06-05 22:12:52 -0500

    refactor: PropertyImportJob perform with helper methods

--- app/jobs/property_import_job.rb
+++ app/jobs/property_import_job.rb
@@ -1,31 +1,40 @@
 require 'csv'
 
 class PropertyImportJob < ApplicationJob
-  # include Sidekiq::Job
-
   queue_as :default
 
+  FILE = Rails.root.join('db', 'data', 'CERT_RENTAL_SUTBLTY.csv')
+
   def perform
-    temp_properties = []
-    file = Rails.root.join("db", "data", "CERT_RENTAL_SUTBLTY.csv")
-    CSV.foreach(file, headers: true) do |row|
-      temp_properties << TempProperty.new(street: row["ADDRESS"], zipcode: row["ZIP"], license_number: license_cleaner(row["LICENSENUMBER"]), license_created_at: row["CRS_CREATEDDATE"])
-    end
-    TempProperty.import temp_properties
-
-    properties = TempProperty.select("DISTINCT ON (street) *").order("street, license_created_at DESC")
-
-    import_properties = properties.map do |property|
-      Property.new(street: property.street, zipcode: property.zipcode, license_number: property.license_number)
-    end
-    import_properties << Property.new(street: "TEST STREET", zipcode: "12345", license_number: "234871035671")
-
-    Property.import import_properties, on_duplicate_key_ignore: [:street]
-
+    TempProperty.import csv_to_temp
+    Property.import temp_to_final(sanitized_list), on_duplicate_key_ignore: [:street]
     TempProperty.delete_all
+  end
+
+  private
+
+  def csv_to_temp
+    temp_properties = []
+    CSV.foreach(FILE, headers: true) do |row|
+      temp_properties << TempProperty.new(street: row['ADDRESS'],
+                                          zipcode: row['ZIP'],
+                                          license_number: license_cleaner(row['LICENSENUMBER']),
+                                          license_created_at: row['CRS_CREATEDDATE'])
+    end
+    temp_properties
   end
 
   def license_cleaner(license_number)
     license_number.strip if license_number.instance_of?(String)
+  end
+
+  def sanitized_list
+    TempProperty.select('DISTINCT ON (street) *').order('street, license_created_at DESC')
+  end
+
+  def temp_to_final(temp_properties)
+    temp_properties.map do |temp_property|
+      Property.new(street: temp_property.street, zipcode: temp_property.zipcode, license_number: temp_property.license_number)
+    end
   end
 end
