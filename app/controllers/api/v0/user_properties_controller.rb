de175a8782b8d9d14b7267bfe5bb43a6fbf2d892
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-06-05 20:21:07 -0600

    chore: delete userproperty functionality

--- app/controllers/api/v0/user_properties_controller.rb
+++ app/controllers/api/v0/user_properties_controller.rb
@@ -9,4 +9,15 @@ class Api::V0::UserPropertiesController < ApplicationController
     PropertySearchFacade.new.set_scores(property)
     render json: PropertySerializer.new(property)
   end
+
+  def destroy
+    user_property = UserProperty.find_by(user_id: params[:user_id], property_id: params[:property_id])
+
+    if user_property.nil?
+      render json: { "errors": [{"detail": "No UserProperty with user_id=#{params[:user_id]} and property_id=#{params[:property_id]} exists"}]},
+             status: 404
+    else
+      user_property.destroy
+    end
+  end
 end
